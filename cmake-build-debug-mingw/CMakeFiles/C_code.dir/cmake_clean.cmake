b253eb6dd156d758f84615226ea8d665b74d5f90
Sun <Hi_sunziyang@163.com>
2022-05-11 11:33:17 +0800

    测试文件

---
+++ cmake-build-debug-mingw/CMakeFiles/C_code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_code.dir/main.c.obj"
+  "CMakeFiles/C_code.dir/main.c.obj.d"
+  "C_code.exe"
+  "C_code.exe.manifest"
+  "C_code.pdb"
+  "libC_code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-mingw/CMakeFiles/C_code.dir/main.c.obj.d
@@ -0,0 +1,10 @@
+CMakeFiles/C_code.dir/main.c.obj: C:\Users\Sun\Desktop\C_code\main.c \
+ D:\Learningsoftware\MinGW\include/stdio.h \
+ D:\Learningsoftware\MinGW\include/_mingw.h \
+ D:\Learningsoftware\MinGW\include/msvcrtver.h \
+ D:\Learningsoftware\MinGW\include/w32api.h \
+ D:\Learningsoftware\MinGW\include/sdkddkver.h \
+ D:\Learningsoftware\MinGW\include/features.h \
+ d:\learningsoftware\mingw\lib\gcc\mingw32\9.2.0\include\stddef.h \
+ D:\Learningsoftware\MinGW\include/sys/types.h \
+ d:\learningsoftware\mingw\lib\gcc\mingw32\9.2.0\include\stdarg.h
